c596f8b992a282a79d8a42503a66db10b734c96e
constuelo <ferguson.rory@gmail.com>
2019-05-17 10:30:17 +0100

    Add image view to admin

--- games/admin.py
+++ games/admin.py
@@ -1,11 +1,21 @@
 # games/admin.py
 from django.contrib import admin
 from . models import Game, Publisher, Developer
+from django.utils.html import mark_safe
 
-
+# @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
-	pass
-    # readonly_fields = ('highlighted',)
+    readonly_fields = ["image_tag",]
+    def image_tag(self, obj):
+        return mark_safe('<img src="{url}" width="{width}" />'.format(
+            url = obj.image.url,
+            width = obj.image.width,
+            )
+        )
+
+# class GameAdmin(admin.ModelAdmin):
+#     fields = ( 'image_tag','title','description','image','externalURL', )
+#     readonly_fields = ('image_tag',)
 
 
 admin.site.register(Game, GameAdmin)
